314bcf8a95220aed1c4af674d75b8912c9f66a00
1zrj <819519226@qq.com>
2024-08-26 15:46:35 +0800

    实现轮播标题动画

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const glide = new Glide(".glide")
+const captionEl = document.querySelectorAll(".slide-caption")
 
+glide.on(["mount.after","run.after"], ()=>{
+    const caption = captionEl[glide.index];
+    anime({
+        targets: caption.children,
+        opacity: [0, 1],
+        duration: 400,
+        easing: "linear",
+        delay: anime.stagger(400, {start: 300}),
+        translateY: [anime.stagger([40, 10]),0]
+    });
+})
+
+glide.on("run.before", () => {
+    document.querySelectorAll(".slide-caption > *").forEach(el => {
+        el.style.opacity = 0;
+    })
+})
 glide.mount();
