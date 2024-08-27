e8f771e25781e4e8b9726ab2ccd786b626c2042a
1zrj <819519226@qq.com>
2024-08-27 11:53:39 +0800

    实现成功案例部分

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const glide = new Glide(".glide")
 const captionEl = document.querySelectorAll(".slide-caption")
 
-glide.on(["mount.after","run.after"], ()=>{
+glide.on(["mount.after", "run.after"], () => {
     const caption = captionEl[glide.index];
     anime({
         targets: caption.children,
         opacity: [0, 1],
         duration: 400,
         easing: "linear",
-        delay: anime.stagger(400, {start: 300}),
-        translateY: [anime.stagger([40, 10]),0]
+        delay: anime.stagger(400, { start: 300 }),
+        translateY: [anime.stagger([40, 10]), 0]
     });
 })
 
@@ -19,3 +19,24 @@ glide.on("run.before", () => {
     })
 })
 glide.mount();
+
+// 成功案例 js
+const isotope = new Isotope(".cases", {
+    layoutMode: "fitRows",
+    itemSelector: ".case-item",
+});
+
+const filterBtns = document.querySelector(".filter-btns");
+
+filterBtns.addEventListener("click", e => {
+    let { target } = e;
+    const filterOption = target.getAttribute("data-filter");
+
+    if (filterOption) {
+        document.querySelectorAll(".filter-btn.active").forEach((btn) => btn.classList.remove("active"));
+        target.classList.add("active");
+
+        isotope.arrange({ filter: filterOption });
+    }
+
+})
